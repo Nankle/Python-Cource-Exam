2a4de27796f6f4ef0785c8cda1b6375246e476c7
Wiki-Chou <72918787+Wiki-Chou@users.noreply.github.com>
2020-11-06 23:00:31 +0800

    可视化路口图片到地图

--- utils/Drawonmap.py
+++ utils/Drawonmap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[4]:
 
 
 #by:WIKI CHOu
@@ -11,22 +11,23 @@ import folium
 import folium.plugins as plugins
 import os
 import numpy as np
+import pandas as pd
 
 
-# In[2]:
+# In[26]:
 
 
 """df 每一行记录代表一个点  包含 lon lat  和picturepath 三个数据"""
 def draw_on_map(df,sav_path):
-    sav_path="t.html"
+    
     
     
     World_map = folium.Map(location=[df['lat'].mean(), df['lon'].mean()], zoom_start=15)
     marker_cluster = plugins.MarkerCluster().add_to(World_map)
-    for index, row in df:
+    for index,row in df.iterrows():
         
    
-        popup = folium.Popup('<img src="'+row['picturepath']+'" alt="'+index+'">')
+        popup = folium.Popup('<img src="'+row['picturepath']+'" alt="'+str(index)+'">')
         folium.Marker([row['lat'], row['lon']], tiles='Stamen Toner',popup=popup).add_to(marker_cluster)     
     #folium.RegularPolygonMarker([row["lat"], row["lon"]], popup="{0}:{1}".format(row["cities"], row["GDP"]),number_of_sides=10,radius=5).add_to(marker_cluster)
     
@@ -34,11 +35,14 @@ def draw_on_map(df,sav_path):
     display(World_map)
     return World_map
 
-
-# In[9]:
-
-
-
+if __name__=='__main__':
+    dic=[{'lat':39.9683939896909 ,'lon':116.288813699693,'picturepath':'1.jpg'},
+    {'lat': 39.9683932702333,'lon':116.289392940432,'picturepath':'2.jpg'}]
+    df=pd.DataFrame(dic)
+    sav_path="t.html"
+    for index,row in df.iterrows():
+        print(index)
+    draw_on_map(df,sav_path)
 
 
 # In[ ]:

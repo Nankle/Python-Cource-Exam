b6c3dae22347eae324137a1547ab1ee83e61adb5
Wiki-Chou <72918787+Wiki-Chou@users.noreply.github.com>
2020-11-11 14:29:40 +0800

    完善注释  添加功能：自动打开浏览器显示结果

--- utils/Drawonmap.py
+++ utils/Drawonmap.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[1]:
 
 
 #by:WIKI CHOu
@@ -12,9 +12,10 @@ import folium.plugins as plugins
 import os
 import numpy as np
 import pandas as pd
+import webbrowser
 
 
-# In[26]:
+# In[2]:
 
 
 """df 每一行记录代表一个点  包含 lon lat  和picturepath 三个数据"""
@@ -22,22 +23,27 @@ def draw_on_map(df,sav_path):
     
     
     
-    World_map = folium.Map(location=[df['lat'].mean(), df['lon'].mean()], zoom_start=15)
-    marker_cluster = plugins.MarkerCluster().add_to(World_map)
-    for index,row in df.iterrows():
+    World_map = folium.Map(location=[df['lat'].mean(), df['lon'].mean()], zoom_start=15)#创建底图，location表示显示地图的中心位置，zoom_start为缩放尺度
+    marker_cluster = plugins.MarkerCluster().add_to(World_map)#marker_cluster 代表一个图层 之后会将点簇添加到这个图层上   相当于在arcgis里面的图层
+    for index,row in df.iterrows():#遍历数据
         
    
-        popup = folium.Popup('<img src="'+row['picturepath']+'" alt="'+str(index)+'">')
-        folium.Marker([row['lat'], row['lon']], tiles='Stamen Toner',popup=popup).add_to(marker_cluster)     
+        popup = folium.Popup('<img src="'+row['picturepath']+'" alt="'+str(index)+'">')#用H5语句创建水滴点的对象
+        folium.Marker([row['lat'], row['lon']], tiles='Stamen Toner',popup=popup).add_to(marker_cluster)#将水滴点添加到图层内     
     #folium.RegularPolygonMarker([row["lat"], row["lon"]], popup="{0}:{1}".format(row["cities"], row["GDP"]),number_of_sides=10,radius=5).add_to(marker_cluster)
     
-    World_map.save(sav_path)
-    # display(World_map)
+    World_map.save(sav_path)#储存整个项目
+
+    webbrowser.open(sav_path)#用默认浏览器打开路径
+    #display(World_map)#展示这个H5
     return World_map
 
 if __name__=='__main__':
     dic=[{'lat':39.9683939896909 ,'lon':116.288813699693,'picturepath':'1.jpg'},
-    {'lat': 39.9683932702333,'lon':116.289392940432,'picturepath':'2.jpg'}]
+    {'lat': 39.9683932702333,'lon':116.289392940432,'picturepath':'2.jpg'}
+        ]
+    for i in range(40):
+        dic.append({'lat': 39.9683932702333+i*0.00001,'lon':116.289392940432,'picturepath':'2.jpg'})
     df=pd.DataFrame(dic)
     sav_path="t.html"
     for index,row in df.iterrows():
